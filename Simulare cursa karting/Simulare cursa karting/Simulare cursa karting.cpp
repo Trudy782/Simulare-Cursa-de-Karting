eef5c4f99a65cfb5342054a1c0f0ff2893032198
Trudix782 <ichimramona782@gmail.com>
2023-12-01 17:56:46 +0200

    Am configurat setările inițiale și am creat o fereastră de vizualizare de dimensiuni fixe.

--- Simulare cursa karting/Simulare cursa karting/Simulare cursa karting.cpp
+++ Simulare cursa karting/Simulare cursa karting/Simulare cursa karting.cpp
@@ -1,20 +1,37 @@
 // Simulare cursa karting.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <stdio.h>
+#include <math.h> 
+
+#include <GL/glew.h>
+
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
